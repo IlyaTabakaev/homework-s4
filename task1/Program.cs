db05a05f66e65ec23e0e4974cd03a45a98637415
Ilya <osetia9595@gmail.com>
2023-01-10 14:11:22 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("input number A");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("input number B");
+int numberB = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Math.Pow(numberA, numberB));
