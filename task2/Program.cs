1ab15ac6edb6c902a201d49c938671a1777310f8
Ilya <osetia9595@gmail.com>
2023-01-10 14:25:04 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("input number");
+int number = int.Parse(Console.ReadLine());
+int sum = 0;
+
+while (number > 0)
+{
+sum = sum + number % 10;
+number = number / 10;
+}
+Console.WriteLine(sum);
