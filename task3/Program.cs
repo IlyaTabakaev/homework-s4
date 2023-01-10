7e95d756267971f73d16b01fef310c6b7dac9305
Ilya <osetia9595@gmail.com>
2023-01-10 14:48:42 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+  Console.WriteLine("input number");
+  int number = int.Parse(Console.ReadLine());
+
+  int[] array = new int[number];
+
+  for (int i = 0; i < number; i++)
+  {
+    array[i] = new Random().Next(number+1);
+    Console.Write(array[i] + " ");
+  }
+ Console.WriteLine();
